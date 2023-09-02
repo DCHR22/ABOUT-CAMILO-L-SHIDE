ca4ab78ae529c1e11f20a091bf6abee31c9206e4
Camilo L-Shide <danielcamilorobles95@gmail.com>
2023-09-01 23:08:59 -0600

    Do first 2 views of tab bar

---
+++ ABOUT CAMILO L-SHIDE/IntroductionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  IntroductionViewController.swift
+//  ABOUT CAMILO L-SHIDE
+//
+//  Created by Camilo L-Shide on 01/09/23.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let label = UILabel()
+        let firstProjectEverName = "SSA Test Tool Mobile app for iPhone"
+        label.text = "Daniel Camilo Hernández Robles, best know as Camilo L-Shide is the founder of L-Shide Studios.\n\nDriven by an endless passion for cellphones and music, this young creator started building his tech skills in 2019 being Swift (with UIKit) his preferred programming language.\n\nIn 2023, L-Shide studios developed its first official project for Soundspace Academy named: \(firstProjectEverName)."
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false /* if we don't set this,
+        the constraints won't be applied as they should!!! */
+        label.lineBreakMode = .byWordWrapping
+        view.addSubview(label)
+        label.textAlignment = .center
+
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            label.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            label.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        ])
+       NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+        ])
+    }
+
+
+}
